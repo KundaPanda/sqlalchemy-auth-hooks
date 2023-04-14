e33458fcaf83336a444caff2602f90c927dd49ca
Vojtěch Dohnal <vojdoh@gmail.com>
2023-04-15 00:11:39 +0200

    Starting select hook implementation

--- sqlalchemy_auth_hooks/handler.py
+++ sqlalchemy_auth_hooks/handler.py
@@ -1,6 +1,14 @@
 import abc
 from typing import Any
 
+from sqlalchemy.orm import Mapper
+
+
+class ReferencedEntity:
+    def __init__(self, entity: Mapper, keys: dict[str, Any] | None = None) -> None:
+        self.entity = entity
+        self.keys = keys or {}
+
 
 class SQLAlchemyAuthHandler(abc.ABC):
     """
@@ -23,6 +31,13 @@ class SQLAlchemyAuthHandler(abc.ABC):
 
     @abc.abstractmethod
     async def on_update(self, instance: Any, changes: dict[str, Any]) -> None:
+        """
+        Handle the deletion of an SQLAlchemy model.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def on_select(self, instance: Any) -> bool:
         """
         Handle the deletion of an SQLAlchemy model.
         """
--- sqlalchemy_auth_hooks/orm_hooks.py
+++ sqlalchemy_auth_hooks/orm_hooks.py
@@ -4,11 +4,11 @@ from collections import defaultdict
 from typing import Any, Generic, TypeVar
 
 import structlog
-from sqlalchemy import event
-from sqlalchemy.orm import InstanceState, Session, UOWTransaction
+from sqlalchemy import BinaryExpression, BindParameter, BooleanClauseList, ColumnClause, Select, Table, event
+from sqlalchemy.orm import InstanceState, Mapper, ORMExecuteState, Session, UOWTransaction
 from structlog.stdlib import BoundLogger
 
-from sqlalchemy_auth_hooks.handler import SQLAlchemyAuthHandler
+from sqlalchemy_auth_hooks.handler import ReferencedEntity, SQLAlchemyAuthHandler
 
 logger: BoundLogger = structlog.get_logger()
 
@@ -16,30 +16,32 @@ _O = TypeVar("_O")
 
 
 class _Hook(abc.ABC, Generic[_O]):
-    def __init__(self, state: InstanceState[_O]) -> None:
-        self.state = state
-
     @abc.abstractmethod
     async def run(self, handler: SQLAlchemyAuthHandler) -> None:
         raise NotImplementedError
+
+
+class _MutationHook(_Hook[_O], abc.ABC):
+    def __init__(self, state: InstanceState[_O]) -> None:
+        self.state = state
 
     def __hash__(self) -> int:
         return hash(self.state)
 
 
-class _CreateHook(_Hook[_O]):
+class _CreateHook(_MutationHook[_O]):
     async def run(self, handler: SQLAlchemyAuthHandler) -> None:
         logger.debug("Create hook called")
         await handler.on_create(self.state.object)
 
 
-class _DeleteHook(_Hook[_O]):
+class _DeleteHook(_MutationHook[_O]):
     async def run(self, handler: SQLAlchemyAuthHandler) -> None:
         logger.debug("Delete hook called")
         await handler.on_delete(self.state.object)
 
 
-class _UpdateHook(_Hook[_O]):
+class _UpdateHook(_MutationHook[_O]):
     def __init__(self, state: InstanceState[_O], changes: dict[str, Any]) -> None:
         super().__init__(state)
         self.changes = changes
@@ -110,6 +112,13 @@ class ORMHooks:
             return
         del self._pending_hooks[session]
 
+    def do_orm_execute(self, orm_execute_state: ORMExecuteState) -> None:
+        logger.debug("do_orm_execute")
+        if orm_execute_state.is_select:
+            entities = _collect_entities(orm_execute_state)
+            for entity in entities:
+                self.handler.on_select(entity)
+
 
 def _register_orm_hooks(handler: SQLAlchemyAuthHandler) -> None:
     """
@@ -121,3 +130,61 @@ def _register_orm_hooks(handler: SQLAlchemyAuthHandler) -> None:
     event.listen(Session, "after_flush_postexec", hooks.after_flush_postexec)
     event.listen(Session, "after_commit", hooks.after_commit)
     event.listen(Session, "after_rollback", hooks.after_rollback)
+    event.listen(Session, "do_orm_execute", hooks.do_orm_execute)
+
+
+def _process_condition(
+    condition: BinaryExpression,
+    mappers: dict[Table, Mapper],
+    intermediate_result: dict[Mapper, ReferencedEntity],
+    parameters: dict[str, Any],
+) -> None:
+    left = condition.left
+    right = condition.right
+
+    if isinstance(left, ColumnClause) and isinstance(right, BindParameter):
+        table = left.table
+        primary_key_columns = table.primary_key.columns.values()
+        if left in primary_key_columns:
+            if isinstance(table, Table) and (mapper := mappers.get(table)):
+                ref_entity = intermediate_result[mapper]
+                key_name = left.name
+                key_value = parameters.get(right.key)
+                if key_value is not None:
+                    ref_entity.keys[key_name] = key_value
+
+
+def _traverse_conditions(
+    condition: BooleanClauseList | BinaryExpression | None,
+    mappers: dict[Table, Mapper],
+    intermediate_result: dict[Mapper, ReferencedEntity],
+    parameters: dict[str, Any],
+) -> None:
+    if condition is not None:
+        if hasattr(condition, "clauses"):
+            for child in condition.clauses:
+                _traverse_conditions(child, mappers, intermediate_result, parameters)
+        else:
+            _process_condition(condition, mappers, intermediate_result, parameters)
+
+
+def _collect_entities(state: ORMExecuteState) -> list[ReferencedEntity]:
+    intermediate_result = {}
+
+    if not isinstance(state.statement, Select):
+        return []
+    select_statement = state.statement
+
+    # Extract mappers from the FROM clause
+    tables = select_statement.get_final_froms()
+    mappers = {mapper.local_table: mapper for mapper in state.all_mappers}
+
+    for table in tables:
+        if mapper := mappers.get(table):
+            intermediate_result[mapper] = ReferencedEntity(entity=mapper, keys={})
+
+    # Extract primary key conditions from the WHERE clause, if any
+    where_clause = select_statement.whereclause
+    _traverse_conditions(where_clause, mappers, intermediate_result, state.parameters or {})
+
+    return list(intermediate_result.values())
