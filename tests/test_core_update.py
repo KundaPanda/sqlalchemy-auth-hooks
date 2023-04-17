d45620a4431519fb245da5a65d387f23c365a2a0
Vojtěch Dohnal <vojdoh@gmail.com>
2023-04-17 13:10:48 +0200

    Add update all test

--- tests/test_core_update.py
+++ tests/test_core_update.py
@@ -1,4 +1,4 @@
-from sqlalchemy import update, inspect
+from sqlalchemy import inspect, update
 from sqlalchemy.orm import Session
 
 from sqlalchemy_auth_hooks.handler import ReferencedEntity
@@ -12,4 +12,14 @@ def test_update(engine, auth_handler, add_user):
     auth_handler.on_update.assert_called_once_with(
         ReferencedEntity(entity=inspect(User), keys={"id": add_user.id}, selectable=User.__table__),
         {"name": "John"},
+    )
+
+
+def test_update_all(engine, auth_handler, add_user):
+    with Session(engine) as session:
+        session.execute(update(User).values(name="John", age=10))
+        session.commit()
+    auth_handler.on_update.assert_called_once_with(
+        ReferencedEntity(entity=inspect(User), selectable=User.__table__),
+        {"name": "John", "age": 10},
     )
