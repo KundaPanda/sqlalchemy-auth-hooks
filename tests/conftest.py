63981206a5852f27ac246f30aee89855d46b98d4
Vojtěch Dohnal <vojdoh@gmail.com>
2023-04-17 09:01:55 +0200

    Split select tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
 import pytest
 from pytest_mock import MockerFixture
-from sqlalchemy import ForeignKey, create_engine
-from sqlalchemy.ext.asyncio import create_async_engine
-from sqlalchemy.orm import Mapped, declarative_base, mapped_column, relationship, backref
+from sqlalchemy import ForeignKey, create_engine, delete
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import Mapped, Session, backref, declarative_base, mapped_column, relationship
 
 from sqlalchemy_auth_hooks.handler import SQLAlchemyAuthHandler
 from sqlalchemy_auth_hooks.hooks import register_hooks
@@ -13,6 +13,123 @@ Base = declarative_base()
 @pytest.fixture(scope="session")
 def anyio_backend():
     return "asyncio"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def engine(worker_id):
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
+    yield engine
+
+
+@pytest.fixture(scope="session")
+async def async_engine(worker_id, anyio_backend):
+    engine = create_async_engine("sqlite+aiosqlite:///:memory:")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield engine
+
+
+@pytest.fixture
+def auth_handler(engine, mocker: MockerFixture):
+    test_handler = mocker.Mock(spec=SQLAlchemyAuthHandler)
+    register_hooks(test_handler)
+    yield test_handler
+
+
+@pytest.fixture
+@pytest.mark.early
+def add_user(engine):
+    user = User(name="John", age=42)
+    with Session(engine) as session:
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        session.expunge(user)
+    return user
+
+
+@pytest.fixture
+@pytest.mark.early
+def user_group(engine, add_user):
+    with Session(engine) as session:
+        group = Group(name="Test Users")
+        session.add(group)
+        session.flush()
+        group.users.append(UserGroup(user_id=add_user.id, group_id=group.id))
+        session.commit()
+        session.refresh(group)
+        session.expunge(group)
+    return group
+
+
+@pytest.fixture
+@pytest.mark.early
+async def add_user_async(anyio_backend, async_engine):
+    user = User(name="John", age=42)
+    async with AsyncSession(async_engine) as session:
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        session.expunge(user)
+    return user
+
+
+@pytest.fixture
+@pytest.mark.early
+async def user_group_async(async_engine, add_user_async):
+    async with AsyncSession(async_engine) as session:
+        group = Group(name="Test Users")
+        session.add(group)
+        await session.flush()
+        await session.refresh(group)
+        ug = UserGroup(user_id=add_user_async.id, group_id=group.id)
+        session.add(ug)
+        await session.commit()
+        await session.refresh(group)
+        session.expunge(group)
+    return group
+
+
+@pytest.fixture(autouse=True)
+def cleanup(engine):
+    yield
+    with Session(engine) as session:
+        session.execute(delete(User).where())
+        session.execute(delete(Group).where())
+        session.execute(delete(UserGroup).where())
+        session.commit()
+
+
+@pytest.fixture(autouse=True)
+async def cleanup_async(anyio_backend, async_engine):
+    yield
+    async with AsyncSession(async_engine) as session:
+        await session.execute(delete(User).where())
+        await session.execute(delete(Group).where())
+        await session.execute(delete(UserGroup).where())
+        await session.commit()
+
+
+@pytest.fixture(autouse=True)
+def reset_handler(auth_handler):
+    auth_handler.reset_mock()
+
+
+def reorder_early_fixtures(metafunc):
+    """
+    Put fixtures with `pytest.mark.early` first during execution
+
+    This allows patch of configurations before the application is initialized
+
+    """
+    for fixture_def in metafunc._arg2fixturedefs.values():  # type: ignore
+        fixture_def = fixture_def[0]
+        for mark in getattr(fixture_def.func, "pytestmark", []):
+            if mark.name == "early":
+                order = metafunc.fixturenames
+                order.insert(0, order.pop(order.index(fixture_def.argname)))
+                break
 
 
 class UserGroup(Base):
@@ -38,44 +155,6 @@ class Group(Base):
     users: Mapped[list["UserGroup"]] = relationship(back_populates="group")
     parent_group_id: Mapped[int | None] = mapped_column(ForeignKey("groups.id"))
     subgroups = relationship("Group", backref=backref("parent_group", remote_side=[id]))
-
-
-@pytest.fixture(scope="session", autouse=True)
-def engine(worker_id):
-    engine = create_engine("sqlite:///:memory:")
-    Base.metadata.create_all(engine)
-    yield engine
-
-
-@pytest.fixture(scope="session")
-async def async_engine(worker_id, anyio_backend):
-    engine = create_async_engine("sqlite+aiosqlite:///:memory:")
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield engine
-
-
-@pytest.fixture
-def auth_handler(engine, mocker: MockerFixture):
-    test_handler = mocker.Mock(spec=SQLAlchemyAuthHandler)
-    register_hooks(test_handler)
-    yield test_handler
-
-
-def reorder_early_fixtures(metafunc):
-    """
-    Put fixtures with `pytest.mark.early` first during execution
-
-    This allows patch of configurations before the application is initialized
-
-    """
-    for fixture_def in metafunc._arg2fixturedefs.values():  # type: ignore
-        fixture_def = fixture_def[0]
-        for mark in getattr(fixture_def.func, "pytestmark", []):
-            if mark.name == "early":
-                order = metafunc.fixturenames
-                order.insert(0, order.pop(order.index(fixture_def.argname)))
-                break
 
 
 def pytest_generate_tests(metafunc):
---
+++ tests/test_orm_select_join.py
@@ -0,0 +1,141 @@
+import pytest
+from sqlalchemy import delete, inspect, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Session, aliased, joinedload, selectinload
+
+from sqlalchemy_auth_hooks.handler import ReferencedEntity
+from tests.conftest import Group, User, UserGroup
+
+
+def test_join(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        session.execute(select(User).join(User.groups).join(UserGroup.group))
+    auth_handler.on_select.assert_called_once_with(
+        [
+            ReferencedEntity(entity=inspect(User), selectable=User.__table__),
+            ReferencedEntity(entity=inspect(UserGroup), selectable=UserGroup.__table__),
+            ReferencedEntity(entity=inspect(Group), selectable=Group.__table__),
+        ]
+    )
+
+
+def test_join_with_where(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        session.execute(select(User).join(User.groups).join(UserGroup.group).where(Group.id == 1))
+    auth_handler.on_select.assert_called_once_with(
+        [
+            ReferencedEntity(entity=inspect(User), selectable=User.__table__),
+            ReferencedEntity(entity=inspect(UserGroup), selectable=UserGroup.__table__),
+            ReferencedEntity(entity=inspect(Group), keys={"id": 1}, selectable=Group.__table__),
+        ]
+    )
+
+
+def test_join_with_condition(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        session.execute(select(User).join(User.groups).join(UserGroup.group.and_(Group.id == 1)))
+    # Although the condition is on the join, treat it as without a condition for now
+    auth_handler.on_select.assert_called_once_with(
+        [
+            ReferencedEntity(entity=inspect(User), selectable=User.__table__),
+            ReferencedEntity(entity=inspect(UserGroup), selectable=UserGroup.__table__),
+            ReferencedEntity(entity=inspect(Group), selectable=Group.__table__),
+        ]
+    )
+
+
+def test_join_recursive(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        session.add(user_group)
+        group = Group(name="Test Users 2")
+        user_group.subgroups.append(group)
+        session.add(group)
+        session.commit()
+        alias = aliased(Group)
+        session.execute(select(Group).join(Group.subgroups.of_type(alias)))
+    # Called when adding the subgroup
+    auth_handler.on_select.assert_any_call([ReferencedEntity(entity=inspect(Group), selectable=Group.__table__)])
+    # Select itself
+    auth_handler.on_select.assert_called_with(
+        [
+            ReferencedEntity(entity=inspect(Group), selectable=Group.__table__),
+            ReferencedEntity(entity=inspect(Group), selectable=alias),
+        ]
+    )
+
+
+def test_join_recursive_with_condition(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        session.add(user_group)
+        group = Group(name="Test Users 2")
+        user_group.subgroups.append(group)
+        session.add(group)
+        session.commit()
+        alias = aliased(Group)
+        session.execute(select(Group).join(Group.subgroups.of_type(alias)).where(alias.id == 2))
+    # Called when adding the subgroup
+    auth_handler.on_select.assert_any_call([ReferencedEntity(entity=inspect(Group), selectable=Group.__table__)])
+    # The select itself
+    auth_handler.on_select.assert_called_with(
+        [
+            ReferencedEntity(entity=inspect(Group), selectable=Group.__table__),
+            ReferencedEntity(entity=inspect(Group), selectable=alias, keys={"id": 2}),
+        ]
+    )
+
+
+def test_join_selectinload(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        session.execute(select(User).options(selectinload(User.groups).selectinload(UserGroup.group))).all()
+    # First, the user table is queried
+    auth_handler.on_select.assert_any_call([ReferencedEntity(entity=inspect(User), selectable=User.__table__)])
+    # Then, a selectinload is performed on the user-groups
+    auth_handler.on_select.assert_any_call(
+        [ReferencedEntity(entity=inspect(UserGroup), selectable=UserGroup.__table__)]
+    )
+    # Finally, a selectinload is performed on the groups as well
+    auth_handler.on_select.assert_called_with([ReferencedEntity(entity=inspect(Group), selectable=Group.__table__)])
+
+
+def test_join_joinedload(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        session.execute(select(User).options(joinedload(User.groups).joinedload(UserGroup.group))).unique().all()
+    # All should be queried at once
+    auth_handler.on_select.assert_called_once()
+    assert len(auth_handler.on_select.call_args_list[0].args[0]) == 3
+    assert auth_handler.on_select.call_args_list[0].args[0][0] == ReferencedEntity(
+        entity=inspect(User), selectable=User.__table__
+    )
+    # Joinedloads have anonymous aliases by default so it's hard to compare them
+    assert auth_handler.on_select.call_args_list[0].args[0][1].entity == inspect(UserGroup)
+    assert auth_handler.on_select.call_args_list[0].args[0][1].selectable.original == UserGroup.__table__
+    assert auth_handler.on_select.call_args_list[0].args[0][2].entity == inspect(Group)
+    assert auth_handler.on_select.call_args_list[0].args[0][2].selectable.original == Group.__table__
+
+
+def test_join_lazyload(engine, add_user, user_group, auth_handler):
+    with Session(engine) as session:
+        user: User = session.scalar(select(User).limit(1))
+        _ = user.groups
+        _ = user.groups[0].group.name
+    assert auth_handler.on_select.call_count == 3
+    auth_handler.on_select.assert_any_call([ReferencedEntity(entity=inspect(User), selectable=User.__table__)])
+    auth_handler.on_select.assert_any_call(
+        [ReferencedEntity(entity=inspect(UserGroup), selectable=UserGroup.__table__)]
+    )
+    # Only one group should be queried
+    auth_handler.on_select.assert_called_with(
+        [ReferencedEntity(entity=inspect(Group), selectable=Group.__table__, keys={"id": 1})]
+    )
+
+
+async def test_join_async(anyio_backend, async_engine, add_user_async, user_group_async, auth_handler):
+    async with AsyncSession(async_engine) as session:
+        await session.execute(select(User).join(User.groups).join(UserGroup.group))
+    auth_handler.on_select.assert_called_once_with(
+        [
+            ReferencedEntity(entity=inspect(User), selectable=User.__table__),
+            ReferencedEntity(entity=inspect(UserGroup), selectable=UserGroup.__table__),
+            ReferencedEntity(entity=inspect(Group), selectable=Group.__table__),
+        ]
+    )
