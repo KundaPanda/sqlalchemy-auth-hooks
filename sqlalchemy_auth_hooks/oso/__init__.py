04ef8e59fe046f38213d444ba28c5245dd303d30
Vojtěch Dohnal <vojdoh@gmail.com>
2023-04-19 10:44:48 +0200

    Updated ruff

--- sqlalchemy_auth_hooks/oso/__init__.py
+++ sqlalchemy_auth_hooks/oso/__init__.py
@@ -4,4 +4,4 @@ except ImportError as e:
     raise RuntimeError("In order to use the `Oso` integration, install this package with the `oso` extra!") from e
 
 
-from .sqlalchemy_oso.auth import authorize_model, register_models, default_polar_model_name
+from .sqlalchemy_oso.auth import authorize_model, default_polar_model_name, register_models
