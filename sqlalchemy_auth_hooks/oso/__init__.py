d77534cc681fc6cf2dd29abc290bf15bf63140ce
Vojtěch Dohnal <vojdoh@gmail.com>
2023-04-19 10:37:17 +0200

    Added basic github actions

--- sqlalchemy_auth_hooks/oso/__init__.py
+++ sqlalchemy_auth_hooks/oso/__init__.py
@@ -2,3 +2,6 @@ try:
     import oso
 except ImportError as e:
     raise RuntimeError("In order to use the `Oso` integration, install this package with the `oso` extra!") from e
+
+
+from .sqlalchemy_oso.auth import authorize_model, register_models, default_polar_model_name
