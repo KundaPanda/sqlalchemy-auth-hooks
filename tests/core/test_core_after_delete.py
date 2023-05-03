281c4eeeace03d72882ee60321e8e4fec326e755
Vojtěch Dohnal <vojdoh@gmail.com>
2023-05-03 08:42:38 +0200

    Fix tests

--- tests/core/test_core_after_delete.py
+++ tests/core/test_core_after_delete.py
@@ -7,26 +7,23 @@ from tests.core.conftest import User
 
 def test_delete(engine, add_user, post_auth_handler, authorized_session):
     with authorized_session as session:
-        u = session.get(User, add_user.id)
-        session.execute(delete(User).where(User.id == u.id))
+        session.execute(delete(User).where(User.id == add_user.id))
         session.commit()
     post_auth_handler.after_many_delete.assert_called_once_with(
         authorized_session,
         ReferencedEntity(entity=inspect(User), selectable=User.__table__),
-        ReferenceConditions(selectable=User.__table__, conditions={"id": {"operator": eq, "value": u.id}}),
+        ReferenceConditions(selectable=User.__table__, conditions={"id": {"operator": eq, "value": add_user.id}}),
     )
 
 
 def test_delete_rollback(engine, add_user, post_auth_handler, authorized_session):
     with authorized_session as session:
-        u = session.get(User, add_user.id)
-        session.delete(u)
+        session.execute(delete(User).where(User.id == add_user.id))
         session.rollback()
-    post_auth_handler.after_single_delete.assert_not_called()
+    post_auth_handler.after_many_delete.assert_not_called()
 
 
 def test_create_rollback_implicit(engine, add_user, post_auth_handler, authorized_session):
     with authorized_session as session:
-        u = session.get(User, add_user.id)
-        session.delete(u)
-    post_auth_handler.after_single_delete.assert_not_called()
+        session.execute(delete(User).where(User.id == add_user.id))
+    post_auth_handler.after_many_delete.assert_not_called()
