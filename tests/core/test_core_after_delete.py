10a8daf38b09cf5eda9179ea61dcf8f09cc9467c
Vojtěch Dohnal <vojdoh@gmail.com>
2023-05-03 09:27:54 +0200

    Added async delete test

--- tests/core/test_core_after_delete.py
+++ tests/core/test_core_after_delete.py
@@ -16,6 +16,17 @@ def test_delete(engine, add_user, post_auth_handler, authorized_session):
     )
 
 
+async def test_delete_async(engine, add_user_async, post_auth_handler, authorized_async_session):
+    async with authorized_async_session as session:
+        await session.execute(delete(User).where(User.id == add_user_async.id))
+        await session.commit()
+    post_auth_handler.after_many_delete.assert_called_once_with(
+        authorized_async_session.sync_session,
+        ReferencedEntity(entity=inspect(User), selectable=User.__table__),
+        ReferenceConditions(selectable=User.__table__, conditions={"id": {"operator": eq, "value": add_user_async.id}}),
+    )
+
+
 def test_delete_rollback(engine, add_user, post_auth_handler, authorized_session):
     with authorized_session as session:
         session.execute(delete(User).where(User.id == add_user.id))
