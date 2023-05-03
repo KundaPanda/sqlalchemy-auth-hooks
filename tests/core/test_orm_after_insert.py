edf6f7f219c99f90706c6034d99abb4103a4e660
Vojtěch Dohnal <vojdoh@gmail.com>
2023-05-03 10:23:12 +0200

    Added async insert test

--- tests/core/test_orm_after_insert.py
+++ tests/core/test_orm_after_insert.py
@@ -10,6 +10,16 @@ def test_create_session(engine, post_auth_handler, authorized_session):
         post_auth_handler.after_single_insert.assert_called_once_with(authorized_session, user)
 
 
+async def test_create_async(async_engine, post_auth_handler, authorized_async_session):
+    user = User(name="John", age=42)
+    async with authorized_async_session as session:
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        assert user.id is not None
+        post_auth_handler.after_single_insert.assert_called_once_with(authorized_async_session.sync_session, user)
+
+
 def test_create_authorization_check(engine, post_auth_handler, authorized_session):
     user = User(name="John", age=42)
     with authorized_session as session:
