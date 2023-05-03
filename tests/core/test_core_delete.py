182271926beb40b55d1f956bc696c237da594bf0
Vojtěch Dohnal <vojdoh@gmail.com>
2023-05-03 10:07:45 +0200

    Added async delete test

--- tests/core/test_core_delete.py
+++ tests/core/test_core_delete.py
@@ -16,6 +16,17 @@ def test_delete(engine, add_user, auth_handler, authorized_session):
     )
 
 
+async def test_delete_async(add_user_async, auth_handler, authorized_async_session):
+    async with authorized_async_session as session:
+        await session.execute(delete(User).where(User.name == "John"))
+        await session.commit()
+    auth_handler.before_delete.assert_called_once_with(
+        authorized_async_session.sync_session,
+        [ReferencedEntity(inspect(User), User.__table__)],
+        ReferenceConditions(selectable=User.__table__, conditions={"name": {"operator": eq, "value": "John"}}),
+    )
+
+
 def test_delete_all(engine, add_user, auth_handler, authorized_session):
     with authorized_session as session:
         session.execute(delete(User).where())
