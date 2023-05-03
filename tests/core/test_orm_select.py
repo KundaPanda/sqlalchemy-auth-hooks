b7db0143cafbe2e2536da477687f2ae1379e9c2b
Vojtěch Dohnal <vojdoh@gmail.com>
2023-05-03 09:26:12 +0200

    Split select tests

--- tests/core/test_orm_select.py
+++ tests/core/test_orm_select.py
@@ -1,5 +1,5 @@
-from sqlalchemy import inspect, select
-from sqlalchemy.sql.operators import and_, eq, or_, startswith_op
+from sqlalchemy import inspect
+from sqlalchemy.sql.operators import and_, eq
 
 from sqlalchemy_auth_hooks.references import (
     CompositeConditions,
@@ -26,80 +26,6 @@ def test_simple_get(engine, add_user, auth_handler, authorized_session):
     )
 
 
-def test_simple_select(engine, add_user, auth_handler, authorized_session):
-    with authorized_session as session:
-        session.execute(select(User).filter_by(id=add_user.id))
-    mapper = inspect(User)
-    selectable = User.__table__
-    auth_handler.before_select.assert_called_once_with(
-        authorized_session,
-        [
-            ReferencedEntity(
-                entity=mapper,
-                selectable=selectable,
-            ),
-        ],
-        ReferenceConditions(selectable, {"id": {"operator": eq, "value": 1}}),
-    )
-
-
-def test_simple_select_where(engine, add_user, auth_handler, authorized_session):
-    with authorized_session as session:
-        session.execute(select(User).where(User.id == add_user.id))
-    mapper = inspect(User)
-    selectable = User.__table__
-    auth_handler.before_select.assert_called_once_with(
-        authorized_session,
-        [
-            ReferencedEntity(
-                entity=mapper,
-                selectable=selectable,
-            ),
-        ],
-        ReferenceConditions(selectable, {"id": {"operator": eq, "value": 1}}),
-    )
-
-
-def test_simple_select_column_only(engine, add_user, auth_handler, authorized_session):
-    with authorized_session as session:
-        session.execute(select(User.name).filter_by(id=add_user.id))
-    mapper = inspect(User)
-    selectable = User.__table__
-    auth_handler.before_select.assert_called_once_with(
-        authorized_session,
-        [
-            ReferencedEntity(
-                entity=mapper,
-                selectable=selectable,
-            ),
-        ],
-        ReferenceConditions(selectable, {"id": {"operator": eq, "value": 1}}),
-    )
-
-
-def test_select_multiple_pk(engine, add_user, user_group, auth_handler, authorized_session):
-    with authorized_session as session:
-        session.execute(select(UserGroup).filter_by(user_id=add_user.id, group_id=user_group.id))
-    mapper = inspect(UserGroup)
-    selectable = UserGroup.__table__
-    auth_handler.before_select.assert_called_once_with(
-        authorized_session,
-        [
-            ReferencedEntity(
-                entity=mapper,
-                selectable=selectable,
-            ),
-        ],
-        CompositeConditions(
-            operator=and_,
-            conditions=[
-                ReferenceConditions(selectable, {"user_id": {"operator": eq, "value": 1}}),
-                ReferenceConditions(selectable, {"group_id": {"operator": eq, "value": 1}}),
-            ],
-        ),
-    )
-
-
 def test_select_get(engine, add_user, user_group, auth_handler, authorized_session):
     with authorized_session as session:
         session.get(UserGroup, (add_user.id, user_group.id))
@@ -118,44 +44,6 @@ def test_select_get(engine, add_user, user_group, auth_handler, authorized_sessi
             conditions=[
                 ReferenceConditions(selectable, {"user_id": {"operator": eq, "value": 1}}),
                 ReferenceConditions(selectable, {"group_id": {"operator": eq, "value": 1}}),
-            ],
-        ),
-    )
-
-
-def test_select_multiple_conditions(engine, add_user, user_group, auth_handler, authorized_session):
-    with authorized_session as session:
-        session.scalars(select(User).where(or_(and_(User.id == add_user.id, User.name.startswith("Jo")), User.id == 2)))
-    mapper = inspect(User)
-    selectable = User.__table__
-    auth_handler.before_select.assert_called_once_with(
-        authorized_session,
-        [
-            ReferencedEntity(
-                entity=mapper,
-                selectable=selectable,
-            ),
-        ],
-        CompositeConditions(
-            operator=or_,
-            conditions=[
-                CompositeConditions(
-                    operator=and_,
-                    conditions=[
-                        ReferenceConditions(
-                            selectable=selectable,
-                            conditions={"id": {"operator": eq, "value": 1}},
-                        ),
-                        ReferenceConditions(
-                            selectable=selectable,
-                            conditions={"name": {"operator": startswith_op, "value": "Jo"}},
-                        ),
-                    ],
-                ),
-                ReferenceConditions(
-                    selectable=selectable,
-                    conditions={"id": {"operator": eq, "value": 2}},
-                ),
             ],
         ),
     )
