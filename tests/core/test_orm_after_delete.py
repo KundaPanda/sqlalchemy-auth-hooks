3c87022bfcd3e3d7b7f6c43132405f00c08a9010
Vojtěch Dohnal <vojdoh@gmail.com>
2023-05-03 10:21:41 +0200

    Added async delete test

--- tests/core/test_orm_after_delete.py
+++ tests/core/test_orm_after_delete.py
@@ -9,6 +9,14 @@ def test_delete(engine, add_user, post_auth_handler, authorized_session):
     post_auth_handler.after_single_delete.assert_called_once_with(authorized_session, u)
 
 
+async def test_delete_async(async_engine, add_user_async, post_auth_handler, authorized_async_session):
+    async with authorized_async_session as session:
+        u = await session.get(User, add_user_async.id)
+        await session.delete(u)
+        await session.commit()
+    post_auth_handler.after_single_delete.assert_called_once_with(authorized_async_session.sync_session, u)
+
+
 def test_delete_rollback(engine, add_user, post_auth_handler, authorized_session):
     with authorized_session as session:
         u = session.get(User, add_user.id)
